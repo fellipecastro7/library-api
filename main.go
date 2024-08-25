75cc2377f7daeb28eb3a6a3b9a0b5b9fb18c289d
fellipecastro7 <fellipemachado77@gmail.com>
2024-08-25 15:27:02 -0300

    feat: chamando container, estabelecendo comunicacao com o banco de dados e inicializando servidor

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"library-api/adapter/http"
+	"library-api/container"
+)
 
+func main() {
+	cont := container.NewContainer()
+
+	r := http.SetupRouter(cont)
+
+	err := r.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
 }
